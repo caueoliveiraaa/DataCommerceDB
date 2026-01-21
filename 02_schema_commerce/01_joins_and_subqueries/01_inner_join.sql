e9c68ab6d845d123fd6925654ac078c19f584466
caueoliveiraaa <77469726+caueoliveiraaa@users.noreply.github.com>
2026-01-21 09:36:35 -0300

    feat: make inner join bring amount of producsts that make sense based on sock amount

--- 02_schema_commerce/01_joins_and_subqueries/01_inner_join.sql
+++ 02_schema_commerce/01_joins_and_subqueries/01_inner_join.sql
@@ -2,19 +2,21 @@
 Purpose: Display a basic inner join.
 Steps:
     - Remove categories that are null.
+    - Find the amount of products that respect the stock amount.
     - Order by name of users.
 */
 SELECT
-    u.name "User name", 
-    u.email "User e-mail",
-    p.name "Product name",
-    p.price "Product price",
-    p.category "Product category",
-    o.quantity "Amonunt bought",
-    s.stock_quantity "Amonunt in stock"
+    u.name user_name, 
+    u.email user_email,
+    p.name product_name,
+    p.price product_price,
+    p.category product_category,
+    o.quantity order_quantity,
+    s.stock_quantity
 FROM commerce.users u
 INNER JOIN commerce.orders o ON o.user_id = u.user_id
 INNER JOIN commerce.products p ON p.product_id = o.product_id
 INNER JOIN commerce.stock s ON s.product_id = p.product_id
 WHERE category IS NOT NULL
+AND o.quantity <= s.stock_quantity
 ORDER BY u.name, u.email;
