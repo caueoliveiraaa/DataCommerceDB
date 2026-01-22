a2bc042811adb26f5669f6f70391363dda2b9bfe
caueoliveiraaa <77469726+caueoliveiraaa@users.noreply.github.com>
2026-01-22 16:58:16 -0300

    feat: add new files

--- 02_schema_commerce/05_triggers/01_update_timestamp.sql
+++ 02_schema_commerce/05_triggers/01_update_timestamp.sql
@@ -3,28 +3,28 @@ Purpose: Updates the column 'updated_at' for all tables when a row is modifed.
 */
 
 -- Function for updating columns
-create trigger update_modified_column()
-return trigger as $$
-begin
-    new.updated_at := now();
-    return new;
-end;
+CREATE TRIGGER update_modified_column()
+RETURN TRIGGER AS $$
+BEGIN
+    new.updated_at := NOW();
+    RETURN NEW;
+END;
 $$ language plpgsql
 
 -- Trigger for table orders
-create trigger trg_orders_updated_at
-before update on orders
-for each row
-execute function update_modified_column();
+CREATE TRIGGER trg_orders_updated_at
+BEFORE UPDATE ON orders
+FOR EACH ROW
+EXECUTE FUNCTION update_modified_column();
 
 -- Trigger for table products
-create trigger trg_products_updated_at
-before update on products
-for each row
-execute function update_modified_column();
+CREATE TRIGGER trg_products_updated_at
+BEFORE UPDATE ON products
+FOR EACH ROW
+EXECUTE FUNCTION update_modified_column();
 
 -- Trigger for table users
-create trigger trg_users_updated_at
-before update on users
-for each row
-execute function update_modified_column();
+CREATE TRIGGER trg_users_updated_at
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE FUNCTION update_modified_column();
