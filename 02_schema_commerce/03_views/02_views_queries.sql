870708669b1f461a1b5e993f8c30a7637669efcb
caueoliveiraaa <77469726+caueoliveiraaa@users.noreply.github.com>
2026-01-23 15:15:13 -0300

    feat: add more views

--- 02_schema_commerce/03_views/02_views_queries.sql
+++ 02_schema_commerce/03_views/02_views_queries.sql
@@ -16,3 +16,9 @@ SELECT * FROM commerce.top_valid_products;
 
 -- View all rows with null records
 SELECT * FROM commerce.null_records;
+
+-- View all orders that each active user has
+SELECT * FROM commerce.all_orders_by_user;
+
+-- View average price per category
+SELECT * FROM commerce.avg_per_category;
