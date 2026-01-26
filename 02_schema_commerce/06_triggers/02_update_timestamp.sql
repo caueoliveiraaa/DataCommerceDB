557aed879cf5d2f8f7fe7b85031e0442077a6209
caueoliveiraaa <77469726+caueoliveiraaa@users.noreply.github.com>
2026-01-26 17:14:42 -0300

    feat: update triggers

--- 02_schema_commerce/06_triggers/02_update_timestamp.sql
+++ 02_schema_commerce/06_triggers/02_update_timestamp.sql
@@ -5,7 +5,6 @@ Steps:
     - Attach the trigger to each table that has a column updated_at.
 */
 
--- First create the trigger function
 CREATE OR REPLACE FUNCTION update_modified_column()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -14,19 +13,16 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- Attach the trigger to the orders table
 CREATE TRIGGER trg_orders_updated_at
 BEFORE UPDATE ON commerce.orders
 FOR EACH ROW
 EXECUTE FUNCTION update_modified_column();
 
--- Attach the trigger to the products table
 CREATE TRIGGER trg_products_updated_at
 BEFORE UPDATE ON commerce.products
 FOR EACH ROW
 EXECUTE FUNCTION update_modified_column();
 
--- Attach the trigger to the users table
 CREATE TRIGGER trg_users_updated_at
 BEFORE UPDATE ON commerce.users
 FOR EACH ROW
