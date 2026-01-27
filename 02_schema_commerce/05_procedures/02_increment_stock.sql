f361c32aa4a1cc21f4427760ffe1954fb7860ba8
caueoliveiraaa <77469726+caueoliveiraaa@users.noreply.github.com>
2026-01-27 08:35:24 -0300

    feat: add function increment stock

--- 02_schema_commerce/05_procedures/02_increment_stock.sql
+++ 02_schema_commerce/05_procedures/02_increment_stock.sql
@@ -7,32 +7,33 @@ CREATE OR REPLACE PROCEDURE commerce.increment_stock(
 	amount_insert INT
 )
 LANGUAGE plpgsql AS $$
-DECLARE 
+DECLARE
 	available_quantity INT;
 BEGIN
-	IF param_product_id IS NULL 
+	IF param_product_id IS NULL
 	OR amount_insert IS NULL
 	OR param_product_id = 0
 	OR amount_insert = 0 THEN
-		RAISE EXCEPTION 'param_product_id and amount_insert cannot be null or zero';
-	END IF;
-
-	SELECT s.stock_quantity INTO available_quantity
-	FROM commerce.stock s
-	WHERE s.product_id = param_product_id;
-
-	IF (available_quantity + amount_insert) > 1000 THEN
-		RAISE EXCEPTION 'The limit available amount per stock is 1000';
+		RAISE EXCEPTION 'Parameters cannot be null or zero';
 	END IF;
 
 	WHILE amount_insert > 0 LOOP
+		SELECT s.stock_quantity INTO available_quantity
+		FROM commerce.stock s
+		WHERE s.product_id = param_product_id;
+		
+		IF available_quantity = 1000 THEN
+			RAISE EXCEPTION 'The limit quantity per stock is 1000';
+		END IF;
+	
 		UPDATE commerce.stock
 		SET stock_quantity = stock_quantity + 1
 		WHERE product_id = param_product_id;
+
 		amount_insert := amount_insert - 1;
 	END LOOP;
 END;
 $$;
 
-CALL commerce.increment_stock(10, 100);
-SELECT stock_quantity FROM commerce.stock WHERE product_id = 10;
+CALL commerce.increment_stock(1, 10);
+SELECT stock_quantity FROM commerce.stock WHERE product_id = 1;
