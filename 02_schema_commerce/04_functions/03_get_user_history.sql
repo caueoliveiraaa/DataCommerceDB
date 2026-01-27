a51a6f2c718c2a629a4a6f2057b0316c8f16555f
caueoliveiraaa <77469726+caueoliveiraaa@users.noreply.github.com>
2026-01-27 08:13:02 -0300

    feat: add function to get the buying history of the user

---
+++ 02_schema_commerce/04_functions/03_get_user_history.sql
@@ -0,0 +1,31 @@
+/*
+Purpose: Create a function that brings the user's buying history.
+*/
+
+CREATE OR REPLACE FUNCTION commerce.get_user_history(
+    param_user_id INT
+)
+RETURNS TABLE(
+	product_name VARCHAR(70),
+	product_price NUMERIC(10, 2),
+	quantity_ordered INT,
+	total_spent_on_product NUMERIC(10, 2)
+) AS $$
+BEGIN
+    IF param_user_id IS NULL
+    OR param_user_id <= 0 THEN
+        RAISE EXCEPTION 'The user id cannot be null or zero';
+    END IF;
+    RETURN QUERY
+    SELECT 
+        p.name,
+        p.price,
+        o.quantity,
+        (p.price * o.quantity) total_spent_on_product
+	FROM commerce.products p
+	INNER JOIN commerce.orders o ON o.product_id = p.product_id
+	WHERE o.user_id = param_user_id;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM commerce.get_user_history(1);
