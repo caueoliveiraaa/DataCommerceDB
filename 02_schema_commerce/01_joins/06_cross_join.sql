3f03fff79cf218c55040e7526d21a43c7cbd50d6
caueoliveiraaa <77469726+caueoliveiraaa@users.noreply.github.com>
2026-03-19 08:46:33 -0300

    feat: update cross join

--- 02_schema_commerce/01_joins/06_cross_join.sql
+++ 02_schema_commerce/01_joins/06_cross_join.sql
@@ -1,6 +1,8 @@
 /*
 Purpose: Display all possible combinations of users and products.
 Steps:
+    - Get all the data from users and products.
+Notes:
     - Use CROSS JOIN to combine every user with every product.
     - This produces a Cartesian product (N users × M products).
 */
