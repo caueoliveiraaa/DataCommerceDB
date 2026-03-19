57768ea203d1bfd59a758597c5ce5179f3014cf3
caueoliveiraaa <77469726+caueoliveiraaa@users.noreply.github.com>
2026-03-19 08:46:26 -0300

    feat: update self join

--- 02_schema_commerce/01_joins/05_self_join.sql
+++ 02_schema_commerce/01_joins/05_self_join.sql
@@ -1,5 +1,11 @@
 /*
 Purpose: Find users with the same name.
+Steps:
+    - Get data from users and orders.
+    - Join the orders with the users via user id.
+    - Order by the user's name.
+Notes:
+    - The self join executes a join on the same table.
 */
 
 SELECT DISTINCT u1.name repeated_names
