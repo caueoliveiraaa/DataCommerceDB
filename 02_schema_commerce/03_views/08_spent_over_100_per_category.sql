7344808aeb39c7512c98b0a157825c1128c558ca
caueoliveiraaa <77469726+caueoliveiraaa@users.noreply.github.com>
2026-03-23 08:34:39 -0300

    doc: document new file for group by with having

--- 02_schema_commerce/03_views/08_spent_over_100_per_category.sql
+++ 02_schema_commerce/03_views/08_spent_over_100_per_category.sql
@@ -1,3 +1,14 @@
+/*
+Purpose: Retrieve how much was spend on each product category but only for
+  groups that spent over 100.
+Steps:
+    - Make sure the view is created without errors.
+    - Select the category and calculate the amount spend on the price column.
+    - Get only categories which are not null.
+    - Group the result of the price sum into each category.
+    - Filter out the groups that spent less than 100.
+    - Order by the total spent, starting from the highest.
+*/
 
 DROP VIEW IF EXISTS commerce.spent_over_100_per_category;
 
